4d1700664da1d9d01b273a043876274e85213ef3
Oleg Chirukhin <oleg@jdk.dev>
2020-10-03 18:13:34 +0300

    add async-await

--- electronapp/src/main/kotlin/ElectronApp.kt
+++ electronapp/src/main/kotlin/ElectronApp.kt
@@ -1,12 +1,37 @@
 import Electron.BrowserWindow
 import Electron.BrowserWindowConstructorOptions
-import Electron.app
+import Electron.App
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+//suspend fun <T> Promise<T>.await(): T = suspendCoroutine { cont ->
+//    then({ cont.resume(it) }, { cont.resumeWithException(it) })
+//}
 
 class ElectronApp(val url: String) {
-    open fun start() {
+    var app: App = Electron.app
+    lateinit var mainWindow: BrowserWindow
+    lateinit var mainWindowUrl: String
+    lateinit var mainWindowPrevUrl: String
+    lateinit var mainWindowNextUrl: String
+
+    suspend fun navigateMainWindow(url: String) {
+        this.mainWindowNextUrl = url
+        this.mainWindow.loadURL(url).await()
+        this.mainWindowPrevUrl = this.mainWindowUrl
+        this.mainWindowUrl = url
+    }
+
+    suspend fun registerApplicationLevelEvents() {
         app.whenReady().then {
             val mainWindow = BrowserWindow(object : BrowserWindowConstructorOptions {})
             mainWindow.loadURL(url)
         }
+    }
+
+    open fun start() {
+        GlobalScope.launch(block = {
+            registerApplicationLevelEvents()
+        });
     }
 }
