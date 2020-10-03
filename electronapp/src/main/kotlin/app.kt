1d4bcb33dffe3a3cd8128f3c3a5523c0f98078dc
Oleg Chirukhin <oleg@jdk.dev>
2020-10-03 03:05:51 +0300

    initial commit

---
+++ electronapp/src/main/kotlin/app.kt
@@ -0,0 +1,17 @@
+import Electron.app
+
+external fun require(module: String): dynamic
+external val process: dynamic
+external val __dirname: dynamic
+
+external class URL(a: String)
+
+fun main(args: Array<String>) {
+
+    console.log("Hello World");
+    console.log(app)
+
+}
+
+
+
