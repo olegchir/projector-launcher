3150023ee71a794ab60482d031cb36b32513b887
Oleg Chirukhin <oleg@jdk.dev>
2020-10-21 18:13:42 +0300

    better building

--- electronapp/src/main/kotlin/GlobalSettings.kt
+++ electronapp/src/main/kotlin/GlobalSettings.kt
@@ -1,6 +1,6 @@
 class GlobalSettings {
     companion object {
         var LOG_LEVEL:String = "debug"
-        var DEVELOPER_TOOLS_ENABLED: Boolean = true
+        var DEVELOPER_TOOLS_ENABLED: Boolean = false
     }
 }
